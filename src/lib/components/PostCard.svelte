d646f1178a34b37062dbbbbf22bdc0a2b884d97b
Kokeh <kokehinc@gmail.com>
2025-11-26 11:20:00 +0100

    feat: add @unpic/svelte for optimized image handling in PostCard component

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Image } from '@unpic/svelte';
 	let { slug, title, excerpt, tags, publishDate, featuredImage } = $props();
 </script>
 
@@ -6,12 +7,15 @@
 	class="flex border-0 rounded-md hover:shadow-lg transition-shadow duration-300 dark:bg-darkMode-card bg-lightMode-card"
 >
 	<a href={`blog/${slug}`} class="flex flex-col sm:flex-row w-full sm:max-h-60">
-		<img
+		<Image
 			src={featuredImage}
 			alt="Post Avatar"
+			width={800}
+			height={600}
 			class="object-cover rounded-t-md sm:rounded-l-sm sm:rounded-t-none sm:min-w-50 h-50 sm:h-full"
 			fetchpriority="high"
 			loading="lazy"
+			layout="constrained"
 		/>
 		<div class="p-4 flex flex-col gap-2">
 			<h3>{title}</h3>
