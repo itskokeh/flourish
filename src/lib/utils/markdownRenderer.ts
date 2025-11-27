90148d06510ab6f0417cc67259e57628b1b6076b
Kokeh <kokehinc@gmail.com>
2025-11-27 23:56:16 +0100

    fix: update markdown-it and add types for improved rendering support

--- src/lib/utils/markdownRenderer.ts
+++ src/lib/utils/markdownRenderer.ts
@@ -1,29 +1,29 @@
 import Shiki from '@shikijs/markdown-it';
+import MarkdownIt from 'markdown-it';
 import markdownAnchor from 'markdown-it-anchor';
-import { MarkdownItAsync } from 'markdown-it-async';
 
-export const renderMarkdown = async (md: string) => {
-	const marked = new MarkdownItAsync({
-		html: true,
-		linkify: true,
-		typographer: true,
+const marked = MarkdownIt({
+	html: true,
+	linkify: true,
+	typographer: true,
+})
+	.use(markdownAnchor, {
+		slugify: (s: string) =>
+			s
+				.trim()
+				.toLowerCase()
+				.replace(/\s+/g, '-')
+				.replace(/[^\w-]/g, ''),
 	})
-		.use(markdownAnchor, {
-			slugify: (s: string) =>
-				s
-					.trim()
-					.toLowerCase()
-					.replace(/\s+/g, '-')
-					.replace(/[^\w-]/g, ''),
-		})
-		.use(
-			await Shiki({
-				themes: {
-					light: 'vitesse-dark',
-					dark: 'vitesse-dark',
-				},
-			}),
-		);
+	.use(
+		await Shiki({
+			themes: {
+				light: 'vitesse-dark',
+				dark: 'vitesse-dark',
+			},
+		}),
+	);
 
-	return await marked.renderAsync(md);
+export const renderMarkdown = (md: string) => {
+	return marked.render(md);
 };
