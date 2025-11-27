7e7f57f899ef3c22a247c101c1439a0a920df740
Kokeh <kokehinc@gmail.com>
2025-11-27 23:26:16 +0100

    fix: refactor markdown rendering to use highlighter for code blocks

--- src/lib/utils/markdownRenderer.ts
+++ src/lib/utils/markdownRenderer.ts
@@ -1,31 +1,38 @@
 import { fromAsyncCodeToHtml } from '@shikijs/markdown-it/async';
 import markdownAnchor from 'markdown-it-anchor';
 import { MarkdownItAsync } from 'markdown-it-async';
-import { codeToHtml } from 'shiki';
+import { createHighlighter } from 'shiki';
+// Import specific packages
+import js from 'shiki/langs/javascript.mjs';
+import svelte from 'shiki/langs/svelte.mjs';
 import darkTheme from 'shiki/themes/vitesse-dark.mjs';
 
-const marked = new MarkdownItAsync({
-	html: true,
-	linkify: true,
-	typographer: true,
-})
-	.use(markdownAnchor, {
-		slugify: (s: string) =>
-			s
-				.trim()
-				.toLowerCase()
-				.replace(/\s+/g, '-')
-				.replace(/[^\w-]/g, ''),
-	})
-	.use(
-		fromAsyncCodeToHtml(codeToHtml, {
-			themes: {
-				light: darkTheme,
-				dark: darkTheme,
-			},
-		}),
-	);
-
 export const renderMarkdown = async (md: string) => {
+	const highlighter = await createHighlighter({
+		themes: [darkTheme],
+		langs: [js, svelte],
+	});
+	const marked = new MarkdownItAsync({
+		html: true,
+		linkify: true,
+		typographer: true,
+	})
+		.use(markdownAnchor, {
+			slugify: (s: string) =>
+				s
+					.trim()
+					.toLowerCase()
+					.replace(/\s+/g, '-')
+					.replace(/[^\w-]/g, ''),
+		})
+		.use(
+			fromAsyncCodeToHtml(async (code, options) => highlighter.codeToHtml(code, options), {
+				themes: {
+					light: 'vitesse-dark',
+					dark: 'vitesse-dark',
+				},
+			}),
+		);
+
 	return await marked.renderAsync(md);
 };
