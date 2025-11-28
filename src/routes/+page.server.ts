5b1cd2d23df20ded978b3e50fd5be93192c16ff0
Kokeh <kokehinc@gmail.com>
2025-11-28 11:25:50 +0100

    feat: implement metadata fetching for posts and projects

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,9 @@
 export const load = async ({ parent }) => {
 	const parentData = await parent();
+	const allPosts = parentData.posts;
 	const allProjects = parentData.projects;
 
+	const posts = allPosts && 'posts' in allPosts ? allPosts.posts : [];
 	const projects = allProjects && 'projects' in allProjects ? allProjects.projects : [];
-	return { projects };
+	return { posts, projects };
 };
