a9b16102996b774859a36af80e0f6a2d3d406436
Kokeh <kokehinc@gmail.com>
2025-11-29 00:06:13 +0100

    style: left align items on post card

--- src/lib/components/PostCard.svelte
+++ src/lib/components/PostCard.svelte
@@ -5,10 +5,7 @@
 <div
 	class="flex border-0 rounded-md hover:shadow-lg transition-shadow duration-300 dark:bg-darkMode-card bg-lightMode-card h-full"
 >
-	<a
-		href={`blog/${slug}`}
-		class="flex flex-col w-full grow"
-	>
+	<a href={`blog/${slug}`} class="flex flex-col w-full grow">
 		<img
 			src={featuredImage}
 			alt="Post Avatar"
@@ -18,13 +15,11 @@
 			class="aspect-[4/5] h-32 sm:h-56 rounded-t-md"
 		/>
 		<div class="p-4 flex flex-col justify-between gap-4 h-full">
-			<h2
-				class="text-lg md:text-2xl text-center md:text-left dark:text-yellow-400"
-			>
+			<h3 class="dark:text-yellow-400">
 				{title}
-			</h2>
+			</h3>
 			<p class="hidden sm:block">{excerpt}</p>
-			<p class="text-center sm:text-left">{publishDate}</p>
+			<p>{publishDate}</p>
 			<div class="sm:flex sm:flex-wrap items-center gap-2 hidden">
 				{#each tags as tag}
 					<span class="tag">{tag}</span>
