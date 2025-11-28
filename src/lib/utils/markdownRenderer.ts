515f1e16672b25b8b997cb34404033af104366b0
Kokeh <kokehinc@gmail.com>
2025-11-28 11:41:35 +0100

    fix: disable Shikijs

--- src/lib/utils/markdownRenderer.ts
+++ src/lib/utils/markdownRenderer.ts
@@ -1,4 +1,4 @@
-import Shiki from '@shikijs/markdown-it';
+// import Shiki from '@shikijs/markdown-it';
 import markdownAnchor from 'markdown-it-anchor';
 import { MarkdownItAsync } from 'markdown-it-async';
 
@@ -6,23 +6,22 @@ const marked = new MarkdownItAsync({
 	html: true,
 	linkify: true,
 	typographer: true,
-})
-	.use(markdownAnchor, {
-		slugify: (s: string) =>
-			s
-				.trim()
-				.toLowerCase()
-				.replace(/\s+/g, '-')
-				.replace(/[^\w-]/g, ''),
-	})
-	.use(
-		await Shiki({
-			themes: {
-				light: 'vitesse-dark',
-				dark: 'vitesse-dark',
-			},
-		}),
-	);
+}).use(markdownAnchor, {
+	slugify: (s: string) =>
+		s
+			.trim()
+			.toLowerCase()
+			.replace(/\s+/g, '-')
+			.replace(/[^\w-]/g, ''),
+});
+// .use(
+// 	await Shiki({
+// 		themes: {
+// 			light: 'vitesse-dark',
+// 			dark: 'vitesse-dark',
+// 		},
+// 	}),
+// );
 
 export const renderMarkdown = async (md: string) => {
 	return marked.renderAsync(md);
