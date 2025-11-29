8dbe8a0e4a21f8a8cabd6bbc3f13c352cd8c3aaa
Kokeh <kokehinc@gmail.com>
2025-11-29 02:47:18 +0100

    feat: enhance error page with improved layout and social media links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,13 +4,14 @@
 	import GH from '$lib/ui/GH.svelte';
 	import TelegramX from '$lib/ui/TelegramX.svelte';
 	import Email from '$lib/ui/Email.svelte';
+	import LinkedIn from '$lib/ui/LinkedIn.svelte';
 	const date = new Date();
 </script>
 
 <footer
 	class="flex flex-col p-4 mb-20 border-t border-gray-700/50 dark:border-gray-700 w-full m-auto"
 >
-	<div class="p-4 flex gap-8 justify-center">
+	<div class="p-4 flex md:gap-6 gap-4 justify-center">
 		<a
 			href="http://x.com/itskokeh"
 			target="_blank"
@@ -19,7 +20,7 @@
 			class="flex items-center"
 		>
 			<X
-				class="w-10 h-10 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-900 text-gray-800"
+				class="w-10 h-10 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-800 text-black"
 			/>
 		</a>
 		<a
@@ -30,16 +31,16 @@
 			class="flex items-center"
 		>
 			<GH
-				class="w-10 h-10 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-900 text-gray-800"
+				class="w-10 h-10 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-800 text-black"
 			/>
 		</a>
 		<a
 			href="mailto:kokeh@proton.me"
-			aria-label="link to send an email"
+			aria-label="Send an email"
 			class="flex items-center"
 		>
 			<Email
-				class="w-9 h-9 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-900 text-gray-800"
+				class="w-9 h-9 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-800 text-black"
 			/>
 		</a>
 		<a
@@ -49,7 +50,20 @@
 			aria-label="link to Telegram account"
 			class="flex items-center"
 		>
-			<TelegramX class="w-10 h-10 dark:text-gray-300 text-blue-900" />
+			<TelegramX
+				class="w-10 h-10 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-800 text-black"
+			/>
+		</a>
+		<a
+			href="http://linkedin.com/in/itskokeh"
+			target="_blank"
+			rel="noopener noreferrer"
+			aria-label="link to linkedIn account"
+			class="flex items-center"
+		>
+			<LinkedIn
+				class="w-10 h-10 dark:text-gray-300 dark:hover:text-gray-400 hover:text-gray-800 text-black"
+			/>
 		</a>
 	</div>
 	<p class="flex justify-center">
