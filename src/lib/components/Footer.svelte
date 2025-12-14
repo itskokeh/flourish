8edd70377bd2b690e360880b4e5a57fb3c65c98f
Kokeh <kokehinc@gmail.com>
2025-12-14 04:09:24 +0100

    feat: disappear icons on home page

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
 	import { Heart } from '$lib';
 	import { socialLinks } from '$lib/utils/socialLinks';
+	import { page } from '$app/state'; // Import page store
+
 	const date = new Date();
 </script>
 
 <footer
 	class="flex flex-col p-4 mb-20 border-t border-gray-200 dark:border-gray-800 w-full mt-auto"
 >
-	<div class="p-4 flex md:gap-6 gap-4 justify-center">
-		{#each socialLinks as { url, component: Icon, ariaLabel }}
-			<a
-				href={url}
-				class="flex items-center hover:text-lightMode-accent dark:hover:text-darkMode-accent transition-colors"
-				aria-label={ariaLabel}
-				rel="noopener noreferrer"
-				target="_blank"
-			>
-				<Icon class="w-8 h-8 sm:w-10 sm:h-10" />
-			</a>
-		{/each}
-	</div>
+	{#if page.url.pathname !== '/'}
+		<div class="p-4 flex md:gap-6 gap-4 justify-center">
+			{#each socialLinks as { url, component: Icon, ariaLabel }}
+				<a
+					href={url}
+					class="flex items-center hover:text-lightMode-accent dark:hover:text-darkMode-accent transition-colors"
+					aria-label={ariaLabel}
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					<Icon class="w-8 h-8 sm:w-10 sm:h-10" />
+				</a>
+			{/each}
+		</div>
+	{/if}
 
-	<div class="flex flex-col items-center gap-2 text-sm sm:text-base">
+	<div class="flex flex-col items-center gap-2 text-sm sm:text-base mt-4">
 		<p
 			class="flex items-center font-bold text-lightMode-text dark:text-darkMode-text"
 		>
